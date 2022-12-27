6ca0df62ff6e9537e5431596c5b19b182917d16d
Christina Okoduwa <christinaokoduwa@gmail.com>
2022-12-27 15:41:09 -0500

    the first commit to the repo for the simple javascript assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+//Write a function that takes three parameters name, height and country.
+//Write an alert in the function that prints out the inputs.
+//Call the function and pass the three parameter values into it. 
+
+
+let myName = "Christina"; 
+let myHeight = "5 feet and 4 inches";
+let myCountry = "United States";
+
+
+function myInfo(name, height, country){
+    //console.log("Hi my name is " + name +  ". I am " + height + " tall. I live in the " + country + "."); 
+    alert ("Hi my name is " + name +  ". I am " + height + " tall. I live in the " + country + "."); 
+}
+
+//console.log (myInfo(myName, myHeight, myCountry));
+myInfo(myName, myHeight, myCountry);
